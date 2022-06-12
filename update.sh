69c9ffa195794708c437dd2cf49fefe4433778e2
Xinecraft <kinnngg786@gmail.com>
2022-06-12 08:41:38 +0530

    Fixes to update script

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
+git pull
 composer install
 
 php artisan clear
 php artisan cache:clear
 php artisan config:clear
-php artisan migrate
-php artisan db:seed
+php artisan migrate --force
 php artisan queue:restart
 
 npm install
