852a6b87d7abaffebec385932805487c68800066
Shift <shift@laravelshift.com>
2022-07-21 16:44:46 +0000

    Use new `@vite` Blade directive

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,11 +10,11 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;0,700;0,800;0,900;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        @vite('resources/css/app.css')
 
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        @vite('resources/js/app.js')
 
         <script>
             let defaultTheme = '{{ app(\App\Settings\ThemeSettings::class)->color_mode }}'
