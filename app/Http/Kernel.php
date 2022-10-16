8996c2973bfe6e9c965a43924d28b5238d74198e
Xinecraft <kinnngg786@gmail.com>
2022-10-17 02:08:42 +0530

    fix: :art: Revert to redis

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequestsWithRedis::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
         'forbid-banned-user' => \App\Http\Middleware\ForbidBannedUser::class,
