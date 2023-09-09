709b2b69867b0139e936f0046581c843c68c2c8c
Xinecraft <kinnngg786@gmail.com>
2023-09-09 17:44:14 +0530

    Revert to marqueue

--- resources/js/Shared/HeaderBroadcastBar.vue
+++ resources/js/Shared/HeaderBroadcastBar.vue
@@ -1,14 +1,19 @@
 <template>
   <div
     v-if="enabled"
-    class="z-50 flex items-center h-12 font-semibold text-white shadow-sm bg-light-blue-500 marquee-container"
+    class="z-50 flex items-center h-12 font-semibold text-white shadow-sm bg-light-blue-500"
   >
     <a
-      class="w-full mt-2 cursor-pointer marquee-link marquee-text"
+      class="w-full mt-2 cursor-pointer"
       target="_blank"
       :href="broadcastUrl"
     >
-      {{ broadcastText }}
+      <marquee
+        onmouseover="this.stop();"
+        onmouseout="this.start();"
+      >
+        {{ broadcastText }}
+      </marquee>
     </a>
   </div>
 </template>
@@ -16,6 +21,7 @@
 <script setup>
 import { usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
+
 
 // get general settings from Inertia getProps
 const { generalSettings } = usePage().props;
@@ -26,35 +32,5 @@ const broadcastUrl = generalSettings?.header_broadcast_url;
 const enabled = computed(() => {
     return !!generalSettings?.header_broadcast_text;
 });
+
 </script>
-
-<style scoped>
-.marquee-container {
-    overflow: hidden;
-    position: relative;
-}
-
-.marquee-link {
-    text-decoration: none;
-}
-
-.marquee-text {
-    white-space: nowrap;
-    position: absolute;
-    animation: marquee-animation 40s linear infinite;
-    width: 100%;
-}
-
-.marquee-container:hover .marquee-text {
-    animation-play-state: paused;
-}
-
-@keyframes marquee-animation {
-    0% {
-        transform: translateX(100%);
-    }
-    100% {
-        transform: translateX(-100%);
-    }
-}
-</style>
