acf89ebacd749c460a732d9faa19e7c58655567a
Xinecraft <kinnngg786@gmail.com>
2023-09-23 23:26:24 +0530

    increase api limit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
@@ -63,7 +62,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(600)->by(optional($request->user())->id ?: $request->ip());
+            return Limit::perMinute(6000)->by(optional($request->user())->id ?: $request->ip());
         });
 
         RateLimiter::for('chat', function (Request $request) {
