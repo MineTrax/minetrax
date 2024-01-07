54426bc7fc18dd01233813fd8c2daf0471b5a6b5
dusty01534 <143600521+dusty01534@users.noreply.github.com>
2024-01-07 19:49:37 +0900

    Update to passwords.php

--- lang/ja/passwords.php
+++ lang/ja/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'パスワードがリセットされました。',
+    'sent' => 'メールアドレスにリセットリンクを送りました。',
+    'throttled' => '再試行まで少々お待ち下さい。',
+    'token' => 'このパスワードリセットリンクは有効期限が切れています。',
+    'user' => "このメールアドレスでユーザーを見つけられませんでした。",
 
 ];
