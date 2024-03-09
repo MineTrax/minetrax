b84b030eff9102521a9b841a88b037faf6383cf1
Xinecraft <kinnngg786@gmail.com>
2024-03-09 19:12:29 +0530

    Fix 500 error on invalid permissions for social auth

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -14,104 +14,122 @@ use Route;
 
 class SocialAuthController extends Controller
 {
-    public function __construct(protected GeolocationService $geolocationService) {}
+    public function __construct(protected GeolocationService $geolocationService)
+    {
+    }
 
     public function redirect($provider, Request $request)
     {
         if ($request->wantsJson()) {
             $redirectUrl = Socialite::driver($provider)->stateless()->redirect()->getTargetUrl();
+
             return response()->json([
-                'redirect_url' => $redirectUrl
+                'redirect_url' => $redirectUrl,
             ]);
         }
-        return Socialite::driver($provider)->redirect();
+
+        return Socialite::driver($provider)
+            ->redirect();
     }
 
     public function handleCallback($provider, Request $request)
     {
         $socialUser = null;
-        if ($request->wantsJson()) {
-            $socialUser = Socialite::driver($provider)->stateless()->user();
-        } else {
-            $socialUser = Socialite::driver($provider)->user();
-        }
-
-        // If there is no email or name address then error
-        if (!$socialUser->getEmail() && !$socialUser->getName()) {
-            return redirect()->route('login')
-                ->with(['toast' => ['type' => 'danger', 'title' => __('Unable to fetch your email address or name.'), 'milliseconds' => 7000]]);
-        }
-
-        // Check if user already exists
-        $user = User::where('email', $socialUser->getEmail())->first();
-
-
-        $allowAnyProviderAuth = config('auth.any_provider_social_auth');
-        // If user exists and has different provider then return error about it
-        if ($user && !$allowAnyProviderAuth && ($socialUser->getId() != $user->provider_id || $provider != $user->provider_name)) {
-            $providerHelper = $user->provider_name ? ucfirst($user->provider_name) : " with password";
+        try {
             if ($request->wantsJson()) {
-                return response()->json(['message' => __('Provider mismatch. You used a different provider while registration. Maybe try :provider?', ['provider' => $providerHelper])], 422);
+                $socialUser = Socialite::driver($provider)->stateless()->user();
+            } else {
+                $socialUser = Socialite::driver($provider)->user();
             }
-            return redirect()->route('login')
-            ->with(['toast' => ['type' => 'danger', 'title' => __('Provider mismatch. You used a different provider while registration. Maybe try :provider?', ['provider' => $providerHelper]), 'milliseconds' => 7000]]);
-        }
 
-        // else generate and token if wants json else login with session
-        if ($user) {
-            $user->update([
+            // If there is no email or name address then error
+            if (! $socialUser->getEmail() && ! $socialUser->getName()) {
+                return redirect()->route('login')
+                    ->with(['toast' => ['type' => 'danger', 'title' => __('Unable to fetch your email address or name.'), 'milliseconds' => 7000]]);
+            }
+
+            // Check if user already exists
+            $user = User::where('email', $socialUser->getEmail())->first();
+
+            $allowAnyProviderAuth = config('auth.any_provider_social_auth');
+            // If user exists and has different provider then return error about it
+            if ($user && ! $allowAnyProviderAuth && ($socialUser->getId() != $user->provider_id || $provider != $user->provider_name)) {
+                $providerHelper = $user->provider_name ? ucfirst($user->provider_name) : ' with password';
+                if ($request->wantsJson()) {
+                    return response()->json(['message' => __('Provider mismatch. You used a different provider while registration. Maybe try :provider?', ['provider' => $providerHelper])], 422);
+                }
+
+                return redirect()->route('login')
+                    ->with(['toast' => ['type' => 'danger', 'title' => __('Provider mismatch. You used a different provider while registration. Maybe try :provider?', ['provider' => $providerHelper]), 'milliseconds' => 7000]]);
+            }
+
+            // else generate and token if wants json else login with session
+            if ($user) {
+                $user->update([
+                    'last_login_at' => now(),
+                    'last_login_ip' => request()->ip(),
+                ]);
+
+                if ($request->wantsJson()) {
+                    // Generate token
+                    $token = $user->createToken('default');
+
+                    return response()->json([
+                        'token_type' => 'Bearer',
+                        'access_token' => $token->plainTextToken,
+                    ]);
+                } else {
+                    Auth::login($user);
+
+                    return redirect()->route('home');
+                }
+            }
+
+            // Before creating new user check if registration feature is disabled.
+            if (! Route::has('register')) {
+                return redirect()->route('login')
+                    ->with(['toast' => ['type' => 'danger', 'title' => __('New user registration is disabled!'), 'milliseconds' => 5000]]);
+            }
+
+            $countryId = $this->geolocationService->getCountryIdFromIP(request()->ip());
+            // Create user if not exists
+            $data = [
+                'username' => Helper::uniqidReal(),
+                'name' => $socialUser->getName(),
+                'email' => $socialUser->getEmail(),
+                'provider_name' => $provider,
+                'provider_id' => $socialUser->getId(),
+                'password' => null,
+                'country_id' => $countryId,
+                'email_verified_at' => now(),
                 'last_login_at' => now(),
                 'last_login_ip' => request()->ip(),
-            ]);
-
+            ];
+            $user = User::create($data);
+            $user->assignRole(Role::DEFAULT_ROLE_NAME);
+            event(new Registered($user));
+            // Return created user with token if wantsJson else login and redirect
             if ($request->wantsJson()) {
-                // Generate token
                 $token = $user->createToken('default');
+
                 return response()->json([
-                    'token_type'      => 'Bearer',
+                    'message' => __('Registration Successful'),
+                    'data' => $user,
                     'access_token' => $token->plainTextToken,
                 ]);
             } else {
                 Auth::login($user);
+
                 return redirect()->route('home');
             }
-        }
 
-        // Before creating new user check if registration feature is disabled.
-        if (!Route::has("register"))
-        {
+        } catch (\Exception $e) {
+            if ($request->wantsJson()) {
+                return response()->json(['message' => __('Unable to fetch user details from :provider', ['provider' => ucfirst($provider)])], 422);
+            }
+
             return redirect()->route('login')
-            ->with(['toast' => ['type' => 'danger', 'title' => __('New user registration is disabled!'), 'milliseconds' => 5000]]);
-        }
-
-        $countryId = $this->geolocationService->getCountryIdFromIP(request()->ip());
-        // Create user if not exists
-        $data = [
-            'username' => Helper::uniqidReal(),
-            'name' => $socialUser->getName(),
-            'email' => $socialUser->getEmail(),
-            'provider_name' => $provider,
-            'provider_id' => $socialUser->getId(),
-            'password' => null,
-            'country_id' => $countryId,
-            'email_verified_at' => now(),
-            'last_login_at' => now(),
-            'last_login_ip' => request()->ip(),
-        ];
-        $user = User::create($data);
-        $user->assignRole(Role::DEFAULT_ROLE_NAME);
-        event(new Registered($user));
-        // Return created user with token if wantsJson else login and redirect
-        if ($request->wantsJson()) {
-            $token = $user->createToken('default');
-            return response()->json([
-                'message'      => __('Registration Successful'),
-                'data'         => $user,
-                'access_token' => $token->plainTextToken,
-            ]);
-        } else {
-            Auth::login($user);
-            return redirect()->route('home');
+                ->with(['toast' => ['type' => 'danger', 'title' => __('Unable to fetch user details from :provider', ['provider' => ucfirst($provider)]), 'milliseconds' => 7000]]);
         }
     }
 }
