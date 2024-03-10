5a5f6eda269cd31322ea0d7bd65d8812ab3a3077
Xinecraft <kinnngg786@gmail.com>
2024-03-10 14:25:21 +0530

    Build fix

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     const outDir = 'public/build/' + (env.APP_THEME || 'default');
+    const buildDirectory = 'build/' + (env.APP_THEME || 'default');
     return {
         plugins: [
             laravel({
                 input: 'resources/js/app.js',
+                buildDirectory: buildDirectory,
                 ssr: 'resources/js/ssr.js',
                 refresh: true,
             }),
