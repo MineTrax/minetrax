eecc952f6c364153406770b293337046b52b938c
Xinecraft <kinnngg786@gmail.com>
2024-12-19 21:37:28 +0530

    Build

---
+++ public/build/default/assets/InputError-CTw21ycU.js
@@ -0,0 +1,1 @@
+import{m as s,s as t,o as a,d as r,a as o,t as c}from"./app-Dpl9gX7Q.js";const n={class:"text-xs text-red-500"},p={__name:"InputError",props:{message:String},setup(e){return(i,m)=>s((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{p as _};
