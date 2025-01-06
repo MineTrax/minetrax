d9dcd01ece904c8e9efe9133a5eeb1341716df13
Xinecraft <kinnngg786@gmail.com>
2025-01-06 21:42:27 +0530

    backup name fix

--- config/backup.php
+++ config/backup.php
@@ -9,7 +9,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'laravel-backup'),
+        'name' => env('BACKUP_APP_NAME', 'laravel-backup'),
 
         'source' => [
             'files' => [
