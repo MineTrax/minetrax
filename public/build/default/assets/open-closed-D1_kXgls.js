50bceabc64a275e749b45865548514d8522c1126
Xinecraft <kinnngg786@gmail.com>
2025-03-17 05:04:16 +0530

    Chat color fixes

--- public/build/default/assets/open-closed-D1_kXgls.js
+++
@@ -1,1 +0,0 @@
-import{E as i,A as t}from"./app-luBSUb-y.js";let o=Symbol("Context");var l=(n=>(n[n.Open=1]="Open",n[n.Closed=2]="Closed",n[n.Closing=4]="Closing",n[n.Opening=8]="Opening",n))(l||{});function r(){return i(o,null)}function s(n){t(o,n)}export{l as i,r as l,s as t};
