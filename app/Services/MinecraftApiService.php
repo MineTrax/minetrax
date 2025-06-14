15ff75639fb306e069fd6b1cb963e7e17d0932d2
Xinecraft <kinnngg786@gmail.com>
2025-06-14 07:26:14 +0530

    update mojang api

--- app/Services/MinecraftApiService.php
+++ app/Services/MinecraftApiService.php
@@ -9,18 +9,18 @@ use Cache;
 
 class MinecraftApiService
 {
-	public static function playerUsernameToUuid($username)
-	{
-		try {
-			$uuid = Cache::remember('minecraft:uuid:'.$username, 3600, function () use ($username) {
-                $httpResponse = Http::timeout(5)->get("https://api.mojang.com/users/profiles/minecraft/${username}");
+    public static function playerUsernameToUuid($username)
+    {
+        try {
+            $uuid = Cache::remember('minecraft:uuid:' . $username, 3600, function () use ($username) {
+                $httpResponse = Http::timeout(5)->get("https://api.minecraftservices.com/minecraft/profile/lookup/name/{$username}");
                 $json = $httpResponse->json();
                 return $json ? $json['id'] : null;
             });
             return $uuid;
-		} catch (Exception $e) {
+        } catch (Exception $e) {
             Log::warning($e);
-			return null;
-		}
-	}
+            return null;
+        }
+    }
 }
