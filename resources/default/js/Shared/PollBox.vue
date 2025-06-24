19500d7aa3f488efcb616977ba4a10980c27d5cc
Xinecraft <kinnngg786@gmail.com>
2025-06-24 20:56:39 +0530

    vote box fixes

--- resources/default/js/Shared/PollBox.vue
+++ resources/default/js/Shared/PollBox.vue
@@ -1,6 +1,6 @@
 <template>
   <Card v-if="poll">
-    <CardContent class="p-3 sm:px-5">
+    <CardContent class="p-3 space-y-2 sm:px-5">
       <h3
         v-if="!isListing"
         class="font-extrabold text-foreground dark:text-foreground"
@@ -13,7 +13,7 @@
       >
         {{ __("Poll") }} {{ poll.id }}
       </h3>
-      <div class="mt-3 text-foreground dark:text-foreground">
+      <div class="mt-3 text-foreground dark:text-card-foreground">
         <poll
           v-bind="options"
           @addvote="addVote"
