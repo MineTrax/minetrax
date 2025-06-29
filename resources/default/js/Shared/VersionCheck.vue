3520c545028ca3d798bebb909046a0d7470d77c5
Xinecraft <kinnngg786@gmail.com>
2025-06-29 14:53:29 +0530

    better version check card

--- resources/default/js/Shared/VersionCheck.vue
+++ resources/default/js/Shared/VersionCheck.vue
@@ -3,23 +3,19 @@
     v-if="!loading && response && response.is_uptodate === false"
     ref="box"
   >
-    <AlertCard
-          text-color="text-error-800 dark:text-error-500"
-    border-color="border-error-500"
-    >
+    <AlertCard variant="destructive">
       {{ __("Your MineTrax Version is Outdated!") }}
-      <template
-        #body
-      >
-        <div class="text-foreground dark:text-foreground">
-          {{ __("Your current MineTrax version is") }}&nbsp;<span class="text-error-500">{{ response.my_version }}</span>&nbsp;{{ __("while the latest version is") }}&nbsp;<span class="text-success-500">{{ response.latest_version }}</span>. {{ __("Please upgrade to enjoy latest features.") }} <br>
+      <template #body>
+        <div class="text-card-foreground">
+          {{ __("Your current MineTrax version is") }}&nbsp;<span class="text-destructive font-semibold">{{ response.my_version }}</span>&nbsp;{{ __("while the latest version is") }}&nbsp;<span class="text-success-500 font-semibold">{{ response.latest_version }}</span>. {{ __("Please upgrade to enjoy latest features.") }}
+          <br><br>
           <a
-            class="text-primary hover:underline"
+            class="text-primary hover:underline font-medium"
             target="_blank"
             href="https://minetrax.github.io/docs/upgrade"
           >{{ __("Click here to know more.") }}</a>
 
-          <p class="text-xs text-foreground italic mt-2">
+          <p class="text-xs text-muted-foreground italic mt-3">
             {{ __("Note: This box is only visible to Staff Member") }}
           </p>
         </div>
