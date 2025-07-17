d9a920ee1fa47dc5c21b3468c28e06c386f9ac16
Xinecraft <kinnngg786@gmail.com>
2025-07-17 15:10:13 +0530

    Enhance NavDropdown and DropdownLink components: add active state handling for navigation links and refactor button and link rendering to improve styling and consistency using Vue 3's <script setup> syntax.

--- resources/default/js/Components/Navigation/NavDropdown.vue
+++ resources/default/js/Components/Navigation/NavDropdown.vue
@@ -6,6 +6,7 @@
         <NavigationMenuLink as-child>
             <ul class="min-w-[250px] m-3 p-2 border border-border rounded">
                 <jet-dropdown-link v-for="item in items" :key="item.key" class="text-sm whitespace-nowrap"
+                    :active="route().current(item.route, item.route_params)"
                     :href="route(item.route, item.route_params ?? null)" :open-in-new-tab="item.is_open_in_new_tab">
                     {{ __(item.title) }}
                 </jet-dropdown-link>
