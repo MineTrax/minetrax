ec08783d40b81a1ec76a4066fbcc7963ab469683
Xinecraft <kinnngg786@gmail.com>
2025-07-18 10:06:17 +0530

    Refactor LocaleSelector component: replace JetDropdown with new DropdownMenu components for improved structure and styling. Introduce DropdownMenu and related components for better modularity and maintainability.

--- resources/default/js/Components/Navigation/LocaleSelector.vue
+++ resources/default/js/Components/Navigation/LocaleSelector.vue
@@ -1,40 +1,30 @@
 <template>
-  <JetDropdown>
-    <template #trigger>
-      <button
-        v-tippy
-        :title="__('Change Language')"
-        class="flex items-center text-sm font-medium text-foreground hover:text-foreground"
-      >
-        <img
-          :src="`/images/flags/flags-iso/shiny/48/${$page.props.localeIsoCode.toUpperCase()}.png`"
-          class="w-6 mr-2"
-        >
-      </button>
-    </template>
-
-    <template #content>
-      <div class="flex flex-col">
-        <button
-          v-for="(locale) in availableLocales"
-          :key="locale.code"
-          class="flex items-center w-full px-4 py-1 text-sm font-medium text-foreground hover:text-foreground dark:text-foreground dark:hover:text-foreground"
-          @click="setLocale(locale.code)"
-        >
-          <img
-            :src="`/images/flags/flags-iso/shiny/48/${locale.iso_code.toUpperCase()}.png`"
-            class="w-8 mr-2"
-          >
-          <span>{{ locale.display }}</span>
-        </button>
-      </div>
-    </template>
-  </JetDropdown>
+    <DropdownMenu>
+        <DropdownMenuTrigger>
+            <button v-tippy :title="__('Change Language')"
+                class="flex items-center text-sm font-medium text-foreground hover:text-foreground">
+                <img :src="`/images/flags/flags-iso/shiny/48/${$page.props.localeIsoCode.toUpperCase()}.png`"
+                    class="w-6 mr-2">
+            </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent class="min-w-48">
+            <DropdownMenuItem v-for="(locale) in availableLocales" :key="locale.code" @click="setLocale(locale.code)"
+                class="cursor-pointer">
+                <img :src="`/images/flags/flags-iso/shiny/48/${locale.iso_code.toUpperCase()}.png`" class="w-8 mr-2">
+                <span>{{ locale.display }}</span>
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
 </template>
 
 <script setup>
-import JetDropdown from '@/Jetstream/Dropdown.vue';
-import { usePage, useForm } from '@inertiajs/vue3';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from '@/Components/ui/dropdown-menu';
+import { useForm, usePage } from '@inertiajs/vue3';
 import { onMounted } from 'vue';
 
 const form = useForm({
