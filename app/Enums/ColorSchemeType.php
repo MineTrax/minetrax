9df240d26e4eb635da892f97c71b5ebfd96e80c6
Xinecraft <kinnngg786@gmail.com>
2026-03-15 14:41:27 +0530

    color schemes wip

--- app/Enums/ColorSchemeType.php
+++ app/Enums/ColorSchemeType.php
@@ -6,34 +6,14 @@ use BenSampo\Enum\Enum;
 
 /**
  * @method static static SKY()
- * @method static static BLUE()
- * @method static static RED()
- * @method static static ORANGE()
- * @method static static LIME()
- * @method static static GREEN()
- * @method static static TEAL()
- * @method static static INDIGO()
- * @method static static FUCHSIA()
+ * @method static static SAFFRON()
+ * @method static static CAFFIENE()
  */
 final class ColorSchemeType extends Enum
 {
     const SKY = 'sky';
-
-    const BLUE = 'blue';
-
-    const RED = 'red';
-
-    const ORANGE = 'orange';
-
-    const LIME = 'lime';
-
-    const GREEN = 'green';
-
-    const TEAL = 'teal';
-
-    const INDIGO = 'indigo';
-
-    const FUCHSIA = 'fuchsia';
+    const SAFFRON = 'saffron';
+    const CAFFIENE = 'caffeine';
 
     /**
      * Google Fonts URLs needed for each color scheme.
@@ -47,16 +27,14 @@ final class ColorSchemeType extends Enum
                 'https://fonts.googleapis.com/css2?family=Source+Serif+4:wght@400;600;700&display=swap',
                 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap',
             ],
-            self::ORANGE => [
+            self::SAFFRON => [
                 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap',
                 'https://fonts.googleapis.com/css2?family=Lora:wght@400;600;700&display=swap',
                 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&display=swap',
             ],
-            self::RED => [
+            self::CAFFIENE => [
                 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
             ],
-            // Add more schemes here as needed.
-            // Schemes not listed will default to SKY fonts.
         ];
 
         return $map[$scheme] ?? $map[self::SKY];
--- resources/default/js/Pages/Profile/UpdateProfileInformationForm.vue
+++ resources/default/js/Pages/Profile/UpdateProfileInformationForm.vue
@@ -1,246 +1,319 @@
 <template>
-    <form @submit.prevent="updateProfileInformation">
-        <div class="grid grid-cols-6 gap-6">
-            <!-- Profile Photo -->
-            <div v-if="page.props.jetstream.managesProfilePhotos" class="col-span-6 sm:col-span-3">
-                <ImageUpload
-                    id="photo"
-                    name="photo"
-                    :label="__('Avatar')"
-                    :current-url="user.profile_photo_url"
-                    v-model="form.photo"
-                    :error="form.errors.photo"
-                    :removable="!!user.profile_photo_path"
-                    shape="circle"
-                    :preview-class="'h-28 w-28'"
-                    @remove="deletePhoto"
-                />
-            </div>
+  <form @submit.prevent="updateProfileInformation">
+    <div class="grid grid-cols-6 gap-6">
+      <!-- Profile Photo -->
+      <div
+        v-if="page.props.jetstream.managesProfilePhotos"
+        class="col-span-6 sm:col-span-3"
+      >
+        <ImageUpload
+          id="photo"
+          v-model="form.photo"
+          name="photo"
+          :label="__('Avatar')"
+          :current-url="user.profile_photo_url"
+          :error="form.errors.photo"
+          :removable="!!user.profile_photo_path"
+          shape="circle"
+          :preview-class="'h-28 w-28'"
+          @remove="deletePhoto"
+        />
+      </div>
 
-            <!-- Cover Image -->
-            <div class="col-span-6 sm:col-span-3">
-                <ImageUpload
-                    id="coverImage"
-                    name="cover_image"
-                    :label="__('Profile Cover Image')"
-                    :current-url="user.cover_image_url"
-                    v-model="form.cover_image"
-                    :error="form.errors.cover_image"
-                    :removable="!!user.cover_image_path"
-                    shape="rect"
-                    :preview-class="'h-28 w-full'"
-                    @remove="deleteCoverImage"
-                />
-            </div>
+      <!-- Cover Image -->
+      <div class="col-span-6 sm:col-span-3">
+        <ImageUpload
+          id="coverImage"
+          v-model="form.cover_image"
+          name="cover_image"
+          :label="__('Profile Cover Image')"
+          :current-url="user.cover_image_url"
+          :error="form.errors.cover_image"
+          :removable="!!user.cover_image_path"
+          shape="rect"
+          :preview-class="'h-28 w-full'"
+          @remove="deleteCoverImage"
+        />
+      </div>
 
-            <!-- Username -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input id="username" :model-value="user.username" :label="__('Username')" :error="form.errors.username" type="text" name="username" :disabled="true" />
-            </div>
+      <!-- Username -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="username"
+          :model-value="user.username"
+          :label="__('Username')"
+          :error="form.errors.username"
+          type="text"
+          name="username"
+          :disabled="true"
+        />
+      </div>
 
-            <!-- Email -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input id="email" :model-value="user.email" :label="__('Email')" :error="form.errors.email" type="email" name="email" :disabled="true" />
-            </div>
+      <!-- Email -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="email"
+          :model-value="user.email"
+          :label="__('Email')"
+          :error="form.errors.email"
+          type="email"
+          name="email"
+          :disabled="true"
+        />
+      </div>
 
-            <!-- Name -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input id="name" v-model="form.name" :label="__('Name')" :error="form.errors.name" :required="true" autocomplete="name" type="text" name="name" />
-            </div>
+      <!-- Name -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="name"
+          v-model="form.name"
+          :label="__('Name')"
+          :error="form.errors.name"
+          :required="true"
+          autocomplete="name"
+          type="text"
+          name="name"
+        />
+      </div>
 
-            <div class="col-span-6 sm:col-span-3">
-                <x-select
-                    id="profile_photo_source"
-                    v-model="form.profile_photo_source"
-                    name="profile_photo_source"
-                    :error="form.errors.profile_photo_source"
-                    :label="__('Use Avatar from')"
-                    :select-list="{ linked_player: 'Linked Player', gravatar: 'Gravatar' }"
-                    :placeholder="__('Uploaded Photo')"
-                />
-            </div>
+      <div class="col-span-6 sm:col-span-3">
+        <x-select
+          id="profile_photo_source"
+          v-model="form.profile_photo_source"
+          name="profile_photo_source"
+          :error="form.errors.profile_photo_source"
+          :label="__('Use Avatar from')"
+          :select-list="{ linked_player: 'Linked Player', gravatar: 'Gravatar' }"
+          :placeholder="__('Uploaded Photo')"
+        />
+      </div>
 
-            <!-- DOB -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-date-picker id="dob" v-model="form.dob" :label="__('Date of Birth')" :placeholder="__('Select your date of birth')" :error="form.errors.dob" name="dob" />
-            </div>
+      <!-- DOB -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-date-picker
+          id="dob"
+          v-model="form.dob"
+          :label="__('Date of Birth')"
+          :placeholder="__('Select your date of birth')"
+          :error="form.errors.dob"
+          name="dob"
+        />
+      </div>
 
-            <div v-if="form.dob" class="col-span-6 sm:col-span-3 md:mt-9">
-                <x-switch id="show_yob" v-model="form.show_yob" :label="__('Show Year of Birth')" :help="__('Show Year of Birth in your public profile.')" name="show_yob" :error="form.errors.show_yob" />
-            </div>
+      <div
+        v-if="form.dob"
+        class="col-span-6 sm:col-span-3 md:mt-7"
+      >
+        <x-switch
+          id="show_yob"
+          v-model="form.show_yob"
+          :label="__('Show Year of Birth')"
+          :help="__('Show Year of Birth in your public profile.')"
+          name="show_yob"
+          :error="form.errors.show_yob"
+        />
+      </div>
 
-            <!-- Gender -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-select
-                    id="gender"
-                    v-model="form.gender"
-                    name="gender"
-                    :error="form.errors.gender"
-                    :label="__('Gender')"
-                    :placeholder="__('Prefer not to say')"
-                    :select-list="{ m: __('Male'), f: __('Female'), o: __('Others') }"
-                />
-            </div>
+      <!-- Gender -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-select
+          id="gender"
+          v-model="form.gender"
+          name="gender"
+          :error="form.errors.gender"
+          :label="__('Gender')"
+          :placeholder="__('Prefer not to say')"
+          :select-list="{ m: __('Male'), f: __('Female'), o: __('Others') }"
+        />
+      </div>
 
-            <div v-if="form.gender" class="col-span-6 sm:col-span-3">
-                <x-switch id="show_gender" v-model="form.show_gender" :label="__('Show Gender')" :help="__('Show Gender in your public profile.')" name="show_gender" :error="form.errors.show_gender" />
-            </div>
+      <div
+        v-if="form.gender"
+        class="col-span-6 sm:col-span-3"
+      >
+        <x-switch
+          id="show_gender"
+          v-model="form.show_gender"
+          :label="__('Show Gender')"
+          :help="__('Show Gender in your public profile.')"
+          name="show_gender"
+          :error="form.errors.show_gender"
+        />
+      </div>
 
-            <!-- s_discord_username -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input
-                    id="s_discord_username"
-                    v-model="form.s_discord_username"
-                    :label="__('Discord Username')"
-                    :error="form.errors.s_discord_username"
-                    autocomplete="s_discord_username"
-                    type="text"
-                    name="s_discord_username"
-                    :help="__('Eg: username#1234')"
-                />
-            </div>
+      <!-- s_discord_username -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="s_discord_username"
+          v-model="form.s_discord_username"
+          :label="__('Discord Username')"
+          :error="form.errors.s_discord_username"
+          autocomplete="s_discord_username"
+          type="text"
+          name="s_discord_username"
+          :help="__('Eg: username#1234')"
+        />
+      </div>
 
-            <!-- s_steam_profile_url -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input
-                    id="s_steam_profile_url"
-                    v-model="form.s_steam_profile_url"
-                    :label="__('Steam Profile URL')"
-                    :error="form.errors.s_steam_profile_url"
-                    autocomplete="s_steam_profile_url"
-                    type="text"
-                    name="s_steam_profile_url"
-                    :help="__('Eg: https://steamcommunity.com/id/username')"
-                />
-            </div>
+      <!-- s_steam_profile_url -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="s_steam_profile_url"
+          v-model="form.s_steam_profile_url"
+          :label="__('Steam Profile URL')"
+          :error="form.errors.s_steam_profile_url"
+          autocomplete="s_steam_profile_url"
+          type="text"
+          name="s_steam_profile_url"
+          :help="__('Eg: https://steamcommunity.com/id/username')"
+        />
+      </div>
 
-            <!-- s_twitter_url -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input
-                    id="s_twitter_url"
-                    v-model="form.s_twitter_url"
-                    :label="__('Twitter Profile URL')"
-                    :error="form.errors.s_twitter_url"
-                    autocomplete="s_twitter_url"
-                    type="text"
-                    name="s_twitter_url"
-                    :help="__('Eg: https://twitter.com/@username')"
-                />
-            </div>
+      <!-- s_twitter_url -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="s_twitter_url"
+          v-model="form.s_twitter_url"
+          :label="__('Twitter Profile URL')"
+          :error="form.errors.s_twitter_url"
+          autocomplete="s_twitter_url"
+          type="text"
+          name="s_twitter_url"
+          :help="__('Eg: https://twitter.com/@username')"
+        />
+      </div>
 
-            <!-- s_youtube_url -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input
-                    id="s_youtube_url"
-                    v-model="form.s_youtube_url"
-                    :label="__('YouTube URL')"
-                    :error="form.errors.s_youtube_url"
-                    autocomplete="s_youtube_url"
-                    type="text"
-                    name="s_youtube_url"
-                    :help="__('Eg: https://www.youtube.com/minecraft')"
-                />
-            </div>
+      <!-- s_youtube_url -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="s_youtube_url"
+          v-model="form.s_youtube_url"
+          :label="__('YouTube URL')"
+          :error="form.errors.s_youtube_url"
+          autocomplete="s_youtube_url"
+          type="text"
+          name="s_youtube_url"
+          :help="__('Eg: https://www.youtube.com/minecraft')"
+        />
+      </div>
 
-            <!-- s_facebook_url -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input
-                    id="s_facebook_url"
-                    v-model="form.s_facebook_url"
-                    :label="__('Facebook URL')"
-                    :error="form.errors.s_facebook_url"
-                    autocomplete="s_facebook_url"
-                    type="text"
-                    name="s_facebook_url"
-                    help="Eg: https://facebook.com/minecraft"
-                />
-            </div>
+      <!-- s_facebook_url -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="s_facebook_url"
+          v-model="form.s_facebook_url"
+          :label="__('Facebook URL')"
+          :error="form.errors.s_facebook_url"
+          autocomplete="s_facebook_url"
+          type="text"
+          name="s_facebook_url"
+          help="Eg: https://facebook.com/minecraft"
+        />
+      </div>
 
-            <!-- s_twitch_url -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input
-                    id="s_twitch_url"
-                    v-model="form.s_twitch_url"
-                    :label="__('Twitch URL')"
-                    :error="form.errors.s_twitch_url"
-                    autocomplete="s_twitch_url"
-                    type="text"
-                    name="s_twitch_url"
-                    :help="__('Eg: https://www.twitch.tv/minecraft')"
-                />
-            </div>
+      <!-- s_twitch_url -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="s_twitch_url"
+          v-model="form.s_twitch_url"
+          :label="__('Twitch URL')"
+          :error="form.errors.s_twitch_url"
+          autocomplete="s_twitch_url"
+          type="text"
+          name="s_twitch_url"
+          :help="__('Eg: https://www.twitch.tv/minecraft')"
+        />
+      </div>
 
-            <!-- s_tiktok_url -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input
-                    id="s_tiktok_url"
-                    v-model="form.s_tiktok_url"
-                    :label="__('TikTok URL')"
-                    :error="form.errors.s_tiktok_url"
-                    autocomplete="s_tiktok_url"
-                    type="text"
-                    name="s_tiktok_url"
-                    :help="__('Eg: https://www.tiktok.com/@minecraft')"
-                />
-            </div>
+      <!-- s_tiktok_url -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="s_tiktok_url"
+          v-model="form.s_tiktok_url"
+          :label="__('TikTok URL')"
+          :error="form.errors.s_tiktok_url"
+          autocomplete="s_tiktok_url"
+          type="text"
+          name="s_tiktok_url"
+          :help="__('Eg: https://www.tiktok.com/@minecraft')"
+        />
+      </div>
 
-            <!-- s_linkedin_url -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input
-                    id="s_linkedin_url"
-                    v-model="form.s_linkedin_url"
-                    :label="__('LinkedIn URL')"
-                    :error="form.errors.s_linkedin_url"
-                    autocomplete="s_linkedin_url"
-                    type="text"
-                    name="s_linkedin_url"
-                    :help="__('Eg: https://www.linkedin.com/in/minecraft')"
-                />
-            </div>
+      <!-- s_linkedin_url -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="s_linkedin_url"
+          v-model="form.s_linkedin_url"
+          :label="__('LinkedIn URL')"
+          :error="form.errors.s_linkedin_url"
+          autocomplete="s_linkedin_url"
+          type="text"
+          name="s_linkedin_url"
+          :help="__('Eg: https://www.linkedin.com/in/minecraft')"
+        />
+      </div>
 
-            <!-- s_website_url -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-input
-                    id="s_website_url"
-                    v-model="form.s_website_url"
-                    :label="__('Website URL')"
-                    :error="form.errors.s_website_url"
-                    autocomplete="s_website_url"
-                    type="text"
-                    name="s_website_url"
-                    :help="__('Eg: https://my-personal-blog.com')"
-                />
-            </div>
+      <!-- s_website_url -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-input
+          id="s_website_url"
+          v-model="form.s_website_url"
+          :label="__('Website URL')"
+          :error="form.errors.s_website_url"
+          autocomplete="s_website_url"
+          type="text"
+          name="s_website_url"
+          :help="__('Eg: https://my-personal-blog.com')"
+        />
+      </div>
 
-            <!-- about -->
-            <div class="col-span-6 sm:col-span-3">
-                <x-textarea id="about" v-model="form.about" :label="__('About Yourself')" :help="__('Something about yourself in 255 characters.')" :error="form.errors.about" name="about" />
-            </div>
+      <!-- about -->
+      <div class="col-span-6 sm:col-span-3">
+        <x-textarea
+          id="about"
+          v-model="form.about"
+          :label="__('About Yourself')"
+          :help="__('Something about yourself in 255 characters.')"
+          :error="form.errors.about"
+          name="about"
+        />
+      </div>
 
-            <div v-if="page.props.localeSwitcherEnabled" class="col-span-6 sm:col-span-3">
-                <x-select id="locale" v-model="form.locale" name="locale" :error="form.errors.locale" :label="__('Language')" :placeholder="__('Select Language...')" :select-list="availableLocales" />
-            </div>
-        </div>
+      <div
+        v-if="page.props.localeSwitcherEnabled"
+        class="col-span-6 sm:col-span-3"
+      >
+        <x-select
+          id="locale"
+          v-model="form.locale"
+          name="locale"
+          :error="form.errors.locale"
+          :label="__('Language')"
+          :placeholder="__('Select Language...')"
+          :select-list="availableLocales"
+        />
+      </div>
+    </div>
 
-        <div class="flex items-center justify-end pt-6 gap-4">
-            <LoadingButton :loading="form.processing">
-                {{ __("Save") }}
-            </LoadingButton>
-        </div>
-    </form>
+    <div class="flex items-center justify-end pt-6 gap-4">
+      <LoadingButton :loading="form.processing">
+        {{ __("Save") }}
+      </LoadingButton>
+    </div>
+  </form>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import { useForm, usePage, router } from "@inertiajs/vue3";
-import LoadingButton from "@/Components/LoadingButton.vue";
-import XInput from "@/Components/Form/XInput.vue";
-import XSwitch from "@/Components/Form/XSwitch.vue";
-import XSelect from "@/Components/Form/XSelect.vue";
-import XTextarea from "@/Components/Form/XTextarea.vue";
-import XDatePicker from "@/Components/Form/XDatePicker.vue";
 import ImageUpload from "@/Components/Form/ImageUpload.vue";
+import XDatePicker from "@/Components/Form/XDatePicker.vue";
+import XInput from "@/Components/Form/XInput.vue";
+import XSelect from "@/Components/Form/XSelect.vue";
+import XSwitch from "@/Components/Form/XSwitch.vue";
+import XTextarea from "@/Components/Form/XTextarea.vue";
+import LoadingButton from "@/Components/LoadingButton.vue";
 import { useTranslations } from "@/Composables/useTranslations";
+import { router, useForm, usePage } from "@inertiajs/vue3";
+import { onMounted, ref } from "vue";
 
 const { __ } = useTranslations();
 
